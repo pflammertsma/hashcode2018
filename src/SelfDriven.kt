49aa0185dcf595eb9f325c9739878a4e51ddafde
Paul Lammertsma <paul@pixplicity.com>
2018-03-01 20:32:42 +0100

    moved things around a bit

--- src/SelfDriven.kt
+++ src/SelfDriven.kt
@@ -2,7 +2,7 @@ import java.io.File
 import java.io.InputStream
 import java.util.*
 
-class Scenario(header: String) {
+class Scenario(lines: List<String>) {
 
     val rows: Int
     val cols: Int
@@ -16,7 +16,7 @@ class Scenario(header: String) {
     var step = 0
 
     init {
-        val values = header.split(" ")
+        val values = lines[0].split(" ")
         var i = 0
         rows = values[i++].toInt()
         cols = values[i++].toInt()
@@ -26,6 +26,19 @@ class Scenario(header: String) {
         rideCount = values[i++].toInt()
         bonus = values[i++].toInt()
         stepCount = values[i++].toInt()
+
+        (1 until rideCount)
+                .forEach {
+                    addRide(Ride(lines[it]))
+                }
+
+        vehicles.forEach {
+            val vehicle = it
+//            Route(vehicle, )
+            rides.forEach {
+
+            }
+        }
     }
 
     fun addRide(ride: Ride) {
@@ -59,14 +72,20 @@ class Ride(line: String) {
     }
 
     override fun toString(): String {
-        return "Ride(startX=$startX, startY=$startY, endX=$endX, endY=$endY, startT=$startT, endT=$endT, distance=${calcDistance()})"
+        return "Ride(startX=$startX, startY=$startY, endX=$endX, endY=$endY, startT=$startT, endT=$endT, distance=${calcDistance(startX, startY)}})"
     }
 
     fun score(currentStep: Int): Int =
-            0
+            if (currentStep + calcDistance(startX, startY) >= endT) {
+                // If we cannot get there in time, return infinite score
+                Int.MAX_VALUE
+            } else {
+                // Otherwise
+                startT - currentStep
+            }
 
-    fun calcDistance() =
-            Math.abs(endX - startX) + Math.abs(endY - startY)
+    fun calcDistance(curX: Int, curY: Int) =
+            Math.abs(endX - curX) + Math.abs(endY - curY)
 
 }
 
@@ -88,11 +107,55 @@ fun main(args: Array<String>) {
     println(inputString)
 
     val lines = inputString.split("\n")
-    val scenario = Scenario(lines[0])
-    (1 until scenario.rideCount)
-            .forEach {
-                scenario.addRide(Ride(lines[it]))
-            }
+    val scenario = Scenario(lines)
 
     println(scenario)
+}
+
+fun testSubset(): ArrayList<IntArray> {
+    val input = intArrayOf(10, 20, 30, 40, 50)    // input array
+    val k = 3                             // sequence length
+
+    val subsets = ArrayList<IntArray>()
+
+    val s = IntArray(k)                  // here we'll keep indices
+    // pointing to elements in input array
+
+    if (k <= input.size) {
+        // first index sequence: 0, 1, 2, ...
+        run {
+            for (i in 0 until k - 1) {
+                s[i] = i
+            }
+        }
+        subsets.add(getSubset(input, s))
+        while (true) {
+            var i: Int
+            // find position of item that can be incremented
+            i = k - 1
+            while (i >= 0 && s[i] == input.size - k + i) {
+                i--
+            }
+            if (i < 0) {
+                break
+            }
+            s[i]++                    // increment this item
+            ++i
+            while (i < k) {    // fill up remaining items
+                s[i] = s[i - 1] + 1
+                i++
+            }
+            subsets.add(getSubset(input, s))
+        }
+    }
+
+    return subsets
+}
+
+// generate actual subset by index sequence
+internal fun getSubset(input: IntArray, subset: IntArray): IntArray {
+    val result = IntArray(subset.size)
+    for (i in subset.indices)
+        result[i] = input[subset[i]]
+    return result
 }
